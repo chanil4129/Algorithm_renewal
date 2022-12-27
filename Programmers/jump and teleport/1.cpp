82d177c7d28f94f6281b5ea4d8ea9639f002d8f8
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2022-12-27 17:46:14 +0900

    점프와 순간 이동

---
+++ Programmers/jump and teleport/1.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int solution(int n)
+{
+    int ans = 0;
+    while(n>0){
+        if(n&1){
+            ans++;
+        }
+        n=n>>1;
+    }
+
+    return ans;
+}
---
+++ Programmers/jump and teleport/README.md
@@ -0,0 +1,7 @@
+# 1
+
+### 비트 연산
+
+---------------------------
+
+https://school.programmers.co.kr/learn/courses/30/lessons/12980
