60ab6030e8061941a4aff1067df396afeebd4a70
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-01-23 11:40:47 +0900

    타겟 넘버

--- Programmers/target_number/2.cpp
+++ Programmers/target_number/2.cpp
@@ -5,23 +5,20 @@ using namespace std;
 
 int total;
 
-void DFS(vector<int> &numbers, int &target,int sum,int n) {
+void dfs(vector<int> &numbers, int &target,int sum,int n) {
     if(n >= numbers.size()){
         if(sum == target) total++;
         return;
     }
 
-    DFS(numbers, target, sum + numbers[n], n+1);
-    DFS(numbers, target, sum - numbers[n], n+1);
+    dfs(numbers, target, sum + numbers[n], n+1);
+    dfs(numbers, target, sum - numbers[n], n+1);
 }
 
 int solution(vector<int> numbers, int target) {
     int answer = 0;
 
-    DFS(numbers, target, numbers[0] , 1);
-    DFS(numbers, target, -numbers[0], 1);
-
+    dfs(numbers, target, 0, 0);
     answer = total;
-
     return answer;
 }
