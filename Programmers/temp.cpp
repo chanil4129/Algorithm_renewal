d185c60a48b90d02dd8a333a572ab297b0fa7668
Kim Chan Il <43468017+chanil4129@users.noreply.github.com>
2023-01-24 23:29:39 +0900

    더 맵게

--- Programmers/temp.cpp
+++ Programmers/temp.cpp
@@ -1,35 +1,20 @@
 #include <string>
 #include <vector>
-#include <unordered_map>
 #include <algorithm>
 #include <iostream>
 
 using namespace std;
 
-bool cmp(const pair<int,int>&a, const pair<int,int> &b){
-    if(a.second==b.second) return a.first<b.first;
-    return a.second>b.second;
-}
-
-int solution(int k, vector<int> tangerine) {
+int solution(vector<int> scoville, int K) {
     int answer = 0;
-    unordered_map<int, int> m;
-    for(int i=0;i<tangerine.size();i++){
-        m[tangerine[i]]++;
-    }
-    vector<pair<int, int>> v(m.begin(), m.end());
-    sort(v.begin(), v.end(),cmp);
-    for (int i = 0; i < v.size(); i++){
-        cout << v[i].second << '\n';
-    }
-    while (k > 0){
-        k -= v[answer++].second;
-    }
     return answer;
 }
 
-
-int main(){
-    vector<int> t{1,3,2,5,4,5,2,3};
-    solution(6,t);
+int main(void){
+    vector<int> v1{1,2,3,4,5};
+    vector<int> v2{9,8,7};
+    v1=v2;
+    for(int i : v1){
+        cout<<i<<"\n";
+    }
 }
